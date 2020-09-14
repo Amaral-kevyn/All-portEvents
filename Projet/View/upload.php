add4c584f4df06847d5e35f9a476034d364d11c7
kev'974 amaral <al.kevyn@gmail.com>
2020-09-14 15:44:15 +0200

    premiere sauvegarde

---
+++ Projet/View/upload.php
@@ -0,0 +1,10 @@
+<form id='tof' action="" method="post" enctype="multipart/form-data">
+    <label for="picture">Téléchager une image de profile</label>
+    <input type="file" name="picture" id="picture">
+    <p>formats acceptés (jpg, jpeg, png, gif), taille max 2M</p>
+    <p class="text-danger"><?=$error?></p>
+    <input type="submit" value="Envoyer">
+</form>
+
+<?php
+include 'footer.php';
